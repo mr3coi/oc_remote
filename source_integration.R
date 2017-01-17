173309215e26f672edfec2e8e4d8443cb77dd8b3
SJC <SJC@SJC.local>
2017-01-17 16:46:16 +0900

    1. Added 'performance evaluation' part 2. Added and modified code for generating 'marker.mat' from regularized regression results (requires further modification to also accept variable selection results from stepwise AIC)

--- source_integration.R
+++ source_integration.R
@@ -165,6 +165,12 @@ colnames(vars) = c("Recurrence","Platinum_resistance","PFS","OS","Op. Debulking"
 
 
 
+
+
+
+
+
+
 ##### ============================================================================================
 ##### ============================================================================================
 ##### ============== Variables & Arguments Setting =============
@@ -188,7 +194,7 @@ outcome_index = c("CR",
 outcome_idx = list(1,2:3,4:5,6:9,10:11,12:13)
 
 ##### Choose which set of response variable and explanatory variables to analyze
-set_num 	= 2
+set_num 	= 1
 var_num 	= 1
 resp_var 	= outcomes[outcome_idx[[set_num]]]
 resp_var 	= resp_var[resp_var %in% colnames(ova.db.csv)][var_num]
@@ -203,7 +209,7 @@ avoid 		= avoid[avoid != resp.ind]		# keep response variable if included in 'avo
 ova.db.csv.sub = ova.db.csv[,-avoid]	# exclude all variables w/ column indices in 'avoid'
 resp.ind	= which(colnames(ova.db.csv.sub) == resp_var)	# update 'resp.ind'
 
-##### Preprocess : remove NA's	=>	'input'
+##### Preprocess : remove NA's	=>	'input' (final data form)
 row_NA		= apply(ova.db.csv.sub,1,function(v) {sum(is.na(v)) == 0})
 input 		= ova.db.csv.sub[row_NA,]
 
@@ -275,6 +281,145 @@ regular.CV = function(input, resp.ind, k=3, LASSO=T, thres=10^(-5), crit="devian
 ##### ============================================================================================
 
 
+######################################################
+### fitting functions
+######################################################
+
+##### Wrapper for RF algorithm
+# train.dat : dataset for train.dat the machine
+# test.dat	   : dataset for test.dat the machine
+# resp.ind : index of the response variable in 'train.dat' and 'test.dat' datasets
+# marker   : vector indicating which columns to use (1) and not to use (0) as
+#				explanatory variables in 'train.dat' and 'test.dat' datasets
+doRF = function(train.dat, test.dat, resp.ind, marker) {
+	require(caret)
+	resp 	= colnames(train.dat)[resp.ind]
+	vars	= colnames(train.dat)
+	f 		= as.formula( paste(resp, "~", paste(vars[which(marker==1)],collapse="+")) )
+	model 	= train(f, data=train.dat, method="rf")
+#	model 	= train(train.dat[,-resp.ind],as.factor(train.dat[,resp.ind]), method="rf")		# factor here
+	pred	= predict(model,test.dat)
+	AUC 	= AUC::auc( roc(pred, as.factor(test.dat[,resp.ind])) )
+	
+	return(AUC)
+}
+
+##### Wrapper for SVM algorithm
+# kernel : type of kernel function to use
+# etc.	 : refer to 'doRF'
+doSVM = function(train.dat, test.dat, resp.ind, marker, kernel="radial") {
+	require(e1071)
+	resp 	= colnames(train.dat)[resp.ind]
+	vars	= colnames(train.dat)
+	f 		= as.formula(paste(resp, "~", paste(vars[which(marker==1)],collapse="+")))
+	model 	= svm(f, data=train.dat, kernel=kernel, prob=T)
+	pred	= predict(model,test.dat, decision.values=T)
+	AUC 	= AUC::auc( roc(pred, as.factor(test.dat[,resp.ind])) )
+	
+	return(AUC)
+}
+
+##### wrapper for k Nearest Neighbors algorithm
+# NN 	: number of neighbor items
+# etc.	: refer to 'doRF'
+doKNN = function(train.dat, test.dat, resp.ind, marker, NN) {
+	# Using 'caret' package
+	require(caret)
+	resp 	= colnames(train.dat)[resp.ind]
+	vars	= colnames(train.dat)
+	f 		= as.formula(paste(resp, "~", paste(vars[which(marker==1)],collapse="+")))
+	#model 	= train(f, data=train.dat,method="knn3",k=NN)
+	#model 	= train(train.dat[,-5],train.dat[,5],method="knn",k=NN,prob=T)
+	model 	= knn3(f,data=train.dat,k=NN,prob=T)
+	pred	= predict(model,test.dat)
+	pred 	= as.numeric(apply(pred,1,function(v) { colnames(pred)[which.max(v)] }))
+	
+	# Using 'knn' function directly
+# 	require(class)
+#  	pred 	= knn(train.dat[,-5],test.dat[,-5],train.dat[,5],k=NN,prob=T)
+
+ 	AUC 	= AUC::auc( roc(pred, as.factor(test.dat[,resp])) )
+ 	
+ 	return(AUC)
+}
+
+##### Wrapper for Generalized Logistic Regression (glm) function
+# etc.	 : refer to 'doRF'
+doLOGIT = function(train.dat, test.dat, resp.ind, marker) {
+	#require(caret)
+	require(ROCR)
+	resp 	= colnames(train.dat)[resp.ind]
+	vars	= colnames(train.dat)
+	f 		= as.formula(paste(resp, "~", paste(vars[which(marker==1)],collapse="+")))
+# 	model 	= train(f, data=train.dat,method="glm")
+	model 	= glm(f,family=binomial(link=logit),data=train.dat)
+	pred	= predict(model, test.dat, type="response")
+# 	pred 	= prediction(pred, te.grp, label.ordering = NULL)			### TODO fix to remove te.grp
+	AUC 	= AUC::auc( roc(pred, as.factor(test.dat[,resp.ind])) )
+	
+	return(AUC)
+}
+
+
+######################################################
+### Wrapper function
+######################################################
+
+##### Generate a matrix w/ each row containing AUC values for RF, SVM, kNN, LOGIT
+##### 	b/w the response variable and variables w/ indices specified in the
+#####	corresponding column of 'marker.mat'.
+##### CV is done in this function to generate 'train.dat' and 'test.dat'.
+##### Given datasets & 'marker.mat' are preprocessed so that all rows w/ NA in data are removed.
+# input			: input data.frame
+# resp.ind		: Column index of the response variable in both 'train.dat' and 'test.dat'
+# marker.mat	: A matrix w/ each row specifying which variables(cols) are included (1) or not (0)
+# k				: # of folds for CV
+# knn.NN		: # of nearest neighbors for kNN algorithm
+# svm.kernel	: type of kernel function to be used for SVM
+performance = function(input, resp.ind, marker.mat, k, knn.NN=3, svm.kernel='radial') {
+	### Assert that the response variable column is a factor
+	if(!is.factor(input[,resp.ind])) stop("Make sure the column for response variable is a factor.")
+	
+	### Definition
+	result	= list()								# container for computed AUC results
+	algs	= c("RF", "SVM", "kNN", 'LOGISTIC')		# ML algorithms for computing AUC
+ 	
+	##### k-fold CV
+	folds	= createFolds(input[,resp.ind],k=k,returnTrain=F)			# create folds w/ specified k
+
+	for (i in 1:k) {
+		train.dat 	= input[-folds[[i]],]
+		test.dat 	= input[folds[[i]],]
+
+	 	### Create 'res.all' to contain results
+	 	cname = paste(algs, 'AUC', sep='_')
+	 	res.all = matrix(nrow=nrow(marker.mat), ncol=length(algs))
+	 	colnames(res.all) = cname
+	 	rownames(res.all) = apply(marker.mat, 1, function(mm) {paste(which(mm==1), collapse = ',')})
+	 	
+	 	### Call each function to compute AUC's
+	 	for( j in 1:nrow(marker.mat) ) {
+	 		# Console progress indicator
+	 		# cat('/ marker set', j, ' / ', nrow(marker.mat), ' total /')
+	 		
+	 		# Generate AUC values using the above submatrices and store in 'res.all'
+	 		tmp.rf		<- doRF(train.dat, test.dat, resp.ind, marker.mat[j,])
+	 		tmp.svm 	<- doSVM(train.dat, test.dat, resp.ind, marker.mat[j,], kernel=svm.kernel)
+	 		tmp.knn 	<- doKNN(train.dat, test.dat, resp.ind, marker.mat[j,], NN=knn.NN)
+	 		tmp.logit 	<- doLOGIT(train.dat, test.dat, resp.ind, marker.mat[j,])
+	 		res.all[j,] <- c(tmp.rf, tmp.svm, tmp.knn, tmp.logit)
+	 	}
+	 	
+	 	### Store AUC values to 'result'
+ 		result[[i]]	= res.all
+	}
+
+	### Compute mean AUC	
+	result[["MEAN"]] = Reduce("+",result) / k
+	
+	return(result)
+}
+
 
 
 ##### ============================================================================================
@@ -284,8 +429,19 @@ regular.CV = function(input, resp.ind, k=3, LASSO=T, thres=10^(-5), crit="devian
 ##### ============================================================================================
 
 
-# ##### Function call for regularized regression
+##### Function call for regularized regression
 result_auc	= regular.CV(input, resp.ind, crit="auc"); result_auc
 result_dev	= regular.CV(input, resp.ind, crit="dev"); result_dev
 result_cls	= regular.CV(input, resp.ind, crit="class"); result_cls
 result_mae	= regular.CV(input, resp.ind, crit="mae"); result_mae
+
+##### 'marker.mat' generation
+marker.mat 			 = rbind(result_auc$vars,result_dev$vars,result_cls$vars,result_mae$vars)
+colnames(marker.mat) = colnames(input[,-resp.ind])
+rownames(marker.mat) = c("reg_auc","reg_dev","reg_cls","reg_mae")
+marker.mat
+
+##### Run comparison
+eval.result = performance(input, resp.ind, marker.mat, k=5); eval.result
+
+
